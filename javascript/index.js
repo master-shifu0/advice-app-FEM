21c57c94ff5125a26728958ffcfe55657e4da347
Shittu AbdulQadeer <shittuquadri41@gmail.com>
2022-07-02 14:16:35 +0100

    Project Complete

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+const id = document.getElementById("id");
+const adviceText = document.getElementById("advice-text");
+const button = document.getElementById("new-advice");
+
+function getData() {
+	const xhr = new XMLHttpRequest();
+	const url = "https://api.adviceslip.com/advice";
+
+	xhr.open("GET", url);
+	xhr.send();
+
+	xhr.onerror = function () {
+		console.log("Network error");
+	};
+
+	xhr.onprogress = function (e) {
+		console.log(e);
+	};
+
+	xhr.onload = function () {
+		if (xhr.status === 200) {
+			const data = JSON.parse(xhr.responseText);
+			adviceText.textContent = data.slip.advice;
+			id.textContent = data.slip.id;
+		} else if (xhr.status === 404) {
+			console.log("Invalid request");
+		}
+	};
+}
+
+button.addEventListener("click", (e) => {
+	getData();
+});
